5ad1a383d2ec203d5012ca58ef383702684d0911
Adam Dewitt <lowkeycoat1@gmail.com>
2024-02-24 18:01:18 -0600

    just adding the whole code

---
+++ Game.kt
@@ -0,0 +1,35 @@
+fun main () {
+    val options = arrayOf("Rock", "Paper", "Scissors")
+    val gameChoice = getGameChoice(options)
+    val userChoice = getUserChoice(options)
+    printResult(userChoice, gameChoice)
+}
+fun getGameChoice(optionsParam: Array<String>) =
+    optionsParam[(Math.random() * optionsParam.size).toInt()]
+
+fun getUserChoice(optionsParam: Array<String>) : String {
+    var isValidChoice = false
+    var userChoice = " "
+    while (!isValidChoice) {
+        print("Please enter one of the following:")
+        for (item in optionsParam) print(" $item")
+        println(".")
+        val userInput = readLine()
+        if (userInput != null && userInput in optionsParam) {
+            isValidChoice = true
+            userChoice = userInput
+        }
+        if (!isValidChoice) println("You must enter a valid choice.")
+    }
+    return userChoice
+        }
+
+fun printResult (userChoice : String, gameChoice : String) {
+    val result : String
+    if (userChoice == gameChoice) result = "tie"
+    else if (( userChoice == "Rock" && gameChoice == "Scissors") ||
+        ( userChoice == "Paper" && gameChoice == "Rock") ||
+        ( userChoice == "Scissors" && gameChoice == "Paper")) result = "YOU WIN"
+    else result = " YOU LOSE"
+    println("You chose $userChoice.  I chose $gameChoice. $result")
+}
